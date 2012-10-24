fcfaeb72713f3f5ca80d93bb47bdd9904fd9ac71
LV Yuchang <yuchang.lv@gmail.com>
2012-10-25 00:11:43 +0200

    ajouter bibliotheque.js

---
+++ client/js/bibliotheque.js
@@ -0,0 +1,10 @@
+(function(window){
+var biblio = function(){};
+
+biblio.notebookEvent = function(){
+	var btnNotebook = document.getElementById("notebookButton");
+	
+};
+
+window.biblio = biblio;
+})(window);
